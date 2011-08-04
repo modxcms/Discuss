bd5f99d928f0ed20c7604d164d4f521b98688941
Shaun McCormick <shaun@modx.com>
2011-08-04 15:19:20 -0500

    Update build version

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -35,7 +35,7 @@ set_time_limit(0);
 
 define('PKG_NAME','Discuss');
 define('PKG_NAME_LOWER','discuss');
-define('PKG_VERSION','1.0.1');
+define('PKG_VERSION','1.0.2');
 define('PKG_RELEASE','rc1');
 
 /* override with your own defines here (see build.config.sample.php) */
