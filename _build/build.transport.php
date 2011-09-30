9a5cbb6f0dae066f3b5ea076eebb5fc8aa19bebc
Shaun McCormick <shaun@modx.com>
2011-09-30 09:44:18 -0500

    Update build version

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -37,7 +37,7 @@ define('PKG_NAME','Discuss');
 define('PKG_NAME_LOWER','discuss');
 
 /* do not forget to change in discuss.class.php too!! */
-define('PKG_VERSION','1.0.8');
+define('PKG_VERSION','1.0.9');
 define('PKG_RELEASE','pl');
 
 /* override with your own defines here (see build.config.sample.php) */
--- core/components/discuss/model/discuss/discuss.class.php
+++ core/components/discuss/model/discuss/discuss.class.php
@@ -105,7 +105,7 @@ class Discuss {
         }
 
         $this->config = array_merge(array(
-            'version' => '1.0.8-pl',
+            'version' => '1.0.9-pl',
             'assetsUrl' => $assetsUrl,
 			'themesUrl' => $themesUrl,
             'theme' => $theme,
