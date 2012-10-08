f86c287c143beb6a8741315f51bf2a066877fa01
Mark Hamstra <hello@markhamstra.com>
2012-10-08 18:41:59 +0200

    Revert issue

--- core/components/discuss/model/discuss/disthread.class.php
+++ core/components/discuss/model/discuss/disthread.class.php
@@ -465,6 +465,12 @@ class disThread extends xPDOSimpleObject {
                 ));
             }
         }
+        $cc = clone $c;
+        $cc->select(array(
+            'COUNT(*) AS `total`',
+        ));
+        $stmt = $cc->prepare();
+        $response['total'] = $modx->getValue($stmt);
         $c->select($modx->getSelectColumns('disThread','disThread'));
         $c->select(array(
             'board_name' => 'Board.name',
@@ -478,9 +484,6 @@ class disThread extends xPDOSimpleObject {
             'last_post_replies' => 'LastPostThread.replies',
         ));
         $c->sortby($sortBy,$sortDir);
-
-        $response['total'] = $modx->getCount('disThread', $c);
-
         $c->limit($limit,$start);
         if (!$countOnly) {
             $response['results'] = $modx->getCollection('disThread',$c);
