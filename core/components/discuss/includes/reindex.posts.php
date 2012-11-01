f50309fe65fbb763949eeaf2424649715c3716f5
Mike Schell <mike@webprogramming.ca>
2012-11-01 11:51:33 -0400

    update indexing script to work via web w/ refreshes

--- core/components/discuss/includes/reindex.posts.php
+++ core/components/discuss/includes/reindex.posts.php
@@ -1,36 +1,18 @@
+<html>
+<head>
+    <meta http-equiv="refresh" content="5">
+</head>
+<body>
 <?php
-define('IX_LIMIT', 101);
-define('IX_OFFSET', 0);
-
-/**
- * Discuss
- *
- * Copyright 2010-11 by Shaun McCormick <shaun@modx.com>
- *
- * This file is part of Discuss, a native forum for MODx Revolution.
- *
- * Discuss is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * Discuss is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Discuss; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
- *
- * @package discuss
- */
 $mtime = microtime();
 $mtime = explode(' ', $mtime);
 $mtime = $mtime[1] + $mtime[0];
 $tstart = $mtime;
 set_time_limit(0);
 
-$forumsResourceUrl = 'forums/';
+define('IX_OFFSET_DEFAULT', 0);
+define('IX_LIMIT_DEFAULT', 1000);
+define('FORUMS_RESOURCE_URL', 'forums/');
 
 /* override with your own defines here (see build.config.sample.php) */
 include 'config.core.php';
@@ -58,7 +40,16 @@ if (version_compare($modx->version['full_version'], '2.2.1-pl', '>=')) {
     $modx->setDebug(-1);
 }
 
+// get last indexed offset
+$di = $modx->cacheManager->get('discuss_index');
+if(empty($di)) {
+    $di['offset'] = IX_OFFSET_DEFAULT;
+}
+$offset = $di['offset'];
+$limit = isset($_GET['limit']) && is_numeric($_GET['limit']) && $_GET['limit'] > 0 ? $_GET['limit'] : IX_LIMIT_DEFAULT;
+
 /* load Discuss */
+$modx->addPackage('discuss', $modx->getOption('discuss.core_path',null,$modx->getOption('core_path').'components/discuss/').'model/discuss/');
 $discuss = $modx->getService('discuss','Discuss',$modx->getOption('discuss.core_path',null,$modx->getOption('core_path').'components/discuss/').'model/discuss/');
 if (!($discuss instanceof Discuss)) return '';
 
@@ -69,7 +60,7 @@ set_time_limit(0);
 echo '<pre>';
 
 $discuss->loadRequest();
-$discuss->url = $forumsResourceUrl;
+$discuss->url = FORUMS_RESOURCE_URL;
 if (!$discuss->loadSearch()) {
     die('No search class!');
 }
@@ -80,7 +71,7 @@ $c->innerJoin('disBoard','Board');
 $c->innerJoin('disThread','Thread');
 $c->innerJoin('disUser','Author');
 $c->where(array(
-    //'Thread.private' => 0,
+    'Thread.private' => 0,
     'Board.status:!=' => 0,
     //'Thread.answered:=' => 1,
 ));
@@ -93,17 +84,19 @@ $c->select(array(
     'Thread.private AS private',
 ));
 $c->sortby('id','ASC');
-$c->limit(IX_LIMIT, IX_OFFSET);
+$c->limit($limit, $offset);
 
 $count = 0;
 $posts = $modx->getIterator('disPost', $c);
 /** @var disPost $post */
 foreach ($posts as $post) {
-    $modx->log(modX::LOG_LEVEL_INFO, 'Indexing: ' . $count++ . ' ' . $post->get('title') . ' (' . $post->get('id') . ")\n");
+    $modx->log(modX::LOG_LEVEL_INFO, 'Indexing: ' . $count . ' ' . $post->get('title') . ' (' . $post->get('id') . ")\n");
     $response = $post->index();
     if($response instanceof SolrUpdateResponse) {
         $modx->log(modX::LOG_LEVEL_INFO, ' (result): ' . $response->getRawResponse() . "\n");
     }
+    $di['offset'] = $offset + ++$count;
+    $modx->cacheManager->set('discuss_index', $di);
 }
 $discuss->search->commit();
 
@@ -115,6 +108,8 @@ $totalTime= ($tend - $tstart);
 $totalTime= sprintf("%2.4f s", $totalTime);
 
 $modx->log(modX::LOG_LEVEL_INFO,"\nExecution time: {$totalTime}\n");
-
+?>
+</body></html>
+<?php
 @session_write_close();
 die();
