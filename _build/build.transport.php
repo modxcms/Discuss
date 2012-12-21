d442998dd5a8a5e1e2e4d7e04849a87c284cd245
Mark Hamstra <hello@markhamstra.com>
2012-12-22 00:04:12 +0100

    Bumpdiedump

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -37,8 +37,8 @@ define('PKG_NAME','Discuss');
 define('PKG_NAME_LOWER','discuss');
 
 /* do not forget to change in discuss.class.php too!! */
-define('PKG_VERSION','1.0.12');
-define('PKG_RELEASE','dev');
+define('PKG_VERSION','1.1.1');
+define('PKG_RELEASE','pl10');
 
 /* override with your own defines here (see build.config.sample.php) */
 require_once dirname(__FILE__) . '/build.config.php';
