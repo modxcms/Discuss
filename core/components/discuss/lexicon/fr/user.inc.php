7a71c5945f531edf90e4a6bf561564838349a079
Romain <romain@melting-media.com>
2012-12-23 15:25:15 +0100

    First step for French translation

---
+++ core/components/discuss/lexicon/fr/user.inc.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Discuss
+ *
+ * Copyright 2010-11 by Shaun McCormick <shaun@modx.com>
+ *
+ * This file is part of Discuss, a native forum for MODx Revolution.
+ *
+ * Discuss is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Discuss is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Discuss; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package discuss
+ */
+/**
+ * @package discuss
+ * @subpackage lexicon
+ */
+$_lang['discuss.account'] = 'Compte';
+$_lang['discuss.account_awaiting_moderation'] = 'Votre compte est en attente de validation de la part d\'un modérateur du fait que votre adresse IP soit marquée comme « spammer » potenciel.';
+$_lang['discuss.account_banned'] = 'Votre compte a été banni.';
+$_lang['discuss.account_deactivated'] = 'Votre compte a été désactivé.';
+$_lang['discuss.account_edit'] = 'Éditer votre compte';
+$_lang['discuss.account_nonexistent'] = 'Veuillez vous inscrire avant de vous connecter.';
+$_lang['discuss.account_settings'] = 'Paramètres du compte';
+$_lang['discuss.account_unconfirmed'] = 'Veuillez vérifier vos emails pour les instructions à suivre avant de pouvoir vous connecter.';
+$_lang['discuss.age'] = 'Age';
+$_lang['discuss.avatar_service'] = 'Service d\'avatar';
+$_lang['discuss.birthdate'] = 'Date de naissance';
+$_lang['discuss.custom_title'] = 'Titre personnalisé';
+$_lang['discuss.date_registered'] = 'Date d\'inscription';
+$_lang['discuss.duplicate_ip'] = 'Adresse IP « dupliquée ».';
+$_lang['discuss.edit'] = 'Éditer';
+$_lang['discuss.edit_profile_for'] = 'Éditer le profile de [[+user]]';
+$_lang['discuss.email'] = 'Email';
+$_lang['discuss.female'] = 'Féminin';
+$_lang['discuss.forum_profile'] = 'Profile de forum';
+$_lang['discuss.gender'] = 'Genre';
+$_lang['discuss.general_stats'] = 'Statistiques générales de [[+user]]';
+$_lang['discuss.go_away_spambot'] = 'Dégage spambot.';
+$_lang['discuss.ignore_preferences'] = 'Préférences « d\'ignorance »';
+$_lang['discuss.joined'] = 'Rejoint';
+$_lang['discuss.last_active'] = 'Derière activité';
+$_lang['discuss.last_login'] = 'Dernière connexion';
+$_lang['discuss.last_online'] = 'Dernière présence en ligne';
+$_lang['discuss.last_reading'] = 'Dernière lecture';
+$_lang['discuss.layout_preferences'] = 'Préférences d\'affichage';
+$_lang['discuss.location'] = 'Lieu';
+$_lang['discuss.login_err_unknown'] = 'Erreur inconnue lors de la connexion.';
+$_lang['discuss.male'] = 'Masculin';
+$_lang['discuss.name'] = 'Nom';
+$_lang['discuss.name_first'] = 'Prénom';
+$_lang['discuss.name_last'] = 'Nom de famille';
+$_lang['discuss.notifications'] = 'Notifications';
+$_lang['discuss.password'] = 'Mot de passe';
+$_lang['discuss.password_confirm'] = 'Confirmation du mot de passe';
+$_lang['discuss.password_current'] = 'Mot de passe actuel';
+$_lang['discuss.post_count'] = 'Nombre de postes';
+$_lang['discuss.posts'] = 'Postes';
+$_lang['discuss.profile'] = 'Profile';
+$_lang['discuss.provide_current_password'] = 'Vous dvrez indiquer le mot de passe actuel de votre compte avant de pouvoir changer les paramètres.';
+$_lang['discuss.register_conf_banned'] = 'Votre compte a été banni. Veuillez contacter un administrateur si vous pensez que c\'est une erreur.';
+$_lang['discuss.register_conf_deactivated'] = 'Votre compte a été désactivé. Veuillez contacter un administrateur si vous pensez que c\'est une erreur.';
+$_lang['discuss.register_conf_moderated'] = 'Votre compte est actuellement en attente de modération, soit parceque votre adresse IP, email ou nom d\'utilisateur ont été marqués comme spammer.';
+$_lang['discuss.register_err_ns_email'] = 'Veuillez indiquer une adresse email valide.';
+$_lang['discuss.register_err_ns_username'] = 'Veuillez iniquer un nom d\'utilisateur valide.';
+$_lang['discuss.register_err_ns_password'] = 'Veuillez indiquer un mot de passe valide.';
+$_lang['discuss.register_err_password_confirm'] = 'Veuillez confirmer votre mot de passe.';
+$_lang['discuss.register_err_password_match'] = 'Vos mots de passe ne correspondent pas.';
+$_lang['discuss.register_err_username_reserved'] = 'Ce nom d\'utilisateur est réservé pour le système. Veuillez en choisir un autre.';
+$_lang['discuss.registered_msg'] = 'Vous êtes enregistré. Un email de confirmation vous sera rapidement envoyé. Veuillez suivre les instructions de cet email pour activer votre compte.';
+$_lang['discuss.show_email_public'] = 'Afficher votre email publiquement ?';
+$_lang['discuss.show_online_status'] = 'Afficher votre présence en ligne ?';
+$_lang['discuss.signature'] = 'Signature';
+$_lang['discuss.spam_blocked_by_filter'] = 'bloqué par un filtre anti-spam.';
+$_lang['discuss.stats'] = 'Stats';
+$_lang['discuss.threads_started'] = 'Fils créés';
+$_lang['discuss.track'] = '« Piste »';
+$_lang['discuss.user_auth_ae'] = 'Un utilisateur avec ce nom existe déjà. Veuillez en essayer un autre.';
+$_lang['discuss.user_auth_exists_bad_pass'] = 'Vous possedez déjà un compte MODX mais vous utilisez le mauvais mot de passe. Veuillez réessayer.';
+$_lang['discuss.user_err_email'] = 'Veuillez indiquer une adresse email valide.';
+$_lang['discuss.user_err_name_first'] = 'Veuillez indiquer un prénom valide.';
+$_lang['discuss.user_err_name_last'] = 'Veuillez indiquer un nom de famille valide.';
+$_lang['discuss.user_err_nf'] = 'Aucun utilisateur trouvé avec ce nom. Veuillez vérifier l\'orthographe et essayer de nouveau.';
+$_lang['discuss.user_err_password'] = 'Veuillez indiquer un mot de passe valide.';
+$_lang['discuss.user_err_password_confirm'] = 'Veuillez confirmer votre mot de passe.';
+$_lang['discuss.user_err_password_incorrect'] = 'Mot de passe incorrecte.';
+$_lang['discuss.user_err_password_match'] = 'Vos mots de passe ne correspondent pas.';
+$_lang['discuss.user_err_merge_same'] = 'Vous ne pouvez fusionner votre compte actuel avec lui-même.';
+$_lang['discuss.user_err_username'] = 'Veuillez indiquer un nom d\'utilisateur valide.';
+$_lang['discuss.username'] = 'Nom d\'utilisateur';
+$_lang['discuss.view'] = 'Voir';
+$_lang['discuss.website'] = 'Site internet';
+
+
